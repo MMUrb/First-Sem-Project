f2cec9dc7ac6e7b39f1109f5d6dd68d58ca8208c
Rahul <25951036@stu.mmu.ac.uk>
2025-11-03 12:41:04 +0000

    This is the maths part of the code which allows for deposits, withdrawals, viewing balance, and viewing accounts

---
+++ UserAccount.java
@@ -0,0 +1,46 @@
+public class UserAccount
+{
+    private String username;
+    private int pin;
+    private double balance;
+
+    public UserAccount(String username, int pin)
+    {
+        this.username = username;
+        this.pin = pin;
+        this.balance = balance;
+    }
+
+    public String getUsername()
+    {
+        return username;
+    }
+
+    public int getPin()
+    {
+        return pin;
+    }
+
+    public double getBalance()
+    {
+        return balance;
+    }
+
+    public void deposit(double amount)
+    {
+        if (amount > 0 )
+        {
+            balance += amount;
+        }
+    }
+
+    public boolean withdraw(double amount)
+    {
+        if (amount > 0 && amount <= balance)
+        {
+            balance -= amount;
+            return true;
+        }
+        return false;
+    }
+}
